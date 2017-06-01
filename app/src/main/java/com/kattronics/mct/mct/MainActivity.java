4cda24edc404be10fcc30c809784f2ec4f2951d4
Lasse Knudsen <lasse.h.knudsen@gmail.com>
2017-06-01 19:41:16 +0200

    2nd commit

---
+++ app/src/main/java/com/kattronics/mct/mct/MainActivity.java
@@ -0,0 +1,27 @@
+package com.kattronics.mct.mct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    final Button button1 = (Button) View.Find(R.id.button1);
+
+    button1.setOnCl
+
+
+}
